872cc506564eb454dec08d34d60ed027a50475a0
Marco Zocca <marco.zocca@unfoldml.com>
2025-02-02 22:53:48 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apk add file
+
 # we mounted the source repo as a Docker volume in /mnt
 cd /mnt
 
@@ -9,3 +11,5 @@ cabal build --enable-executable-static
 # copy binary
 mkdir out/
 cp $(cabal -v0 list-bin exe:test-app) out/
+
+file out/test-app
